0bfb282da751970f11b8a1b002db8ed2385b950e
Alex Pareto <alex.pareto@gmail.com>
2024-09-28 16:57:51 -0700

    custom port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const Parser = require("rss-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const parser = new Parser();
 
 // Serve static files from the 'public' directory
